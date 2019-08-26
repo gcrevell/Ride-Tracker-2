5a864a58c0947835cdc08147b23ee3533d5dda2d
Gabriel Revells <gcrevell@mtu.edu>
2019-08-26 19:54:00 -0400

    Initial Commit

---
+++ Ride Tracker 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ride Tracker 2
+//
+//  Created by Gabriel Revells on 8/26/19.
+//  Copyright © 2019 Gabriel Revells. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
